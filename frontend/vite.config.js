7243de954c597e65b9bff65260e2f6271afad002
sumer82105 <sumer@8210597901@gmail.com>
2025-06-18 19:48:57 +0530

    vercel config update

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,5 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -11,7 +12,20 @@ export default defineConfig({
   define: {
     global: 'globalThis',
   },
+  build: {
+    outDir: 'dist',
+  },
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
+  },
   optimizeDeps: {
     include: ['sockjs-client']
+  },
+  // This helps Vercel (or Netlify) route properly for SPAs
+  preview: {
+    port: 4173,
+    host: true,
   }
-}) 
+})
